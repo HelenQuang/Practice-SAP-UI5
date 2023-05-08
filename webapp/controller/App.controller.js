9c6d7e223a0764823cdf51efd5b22cbf07aafab6
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-05-08 13:29:42 +0300

    Add i18n to translate text

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,27 +3,40 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  function (Controller, MessageToast, JSONModel) {
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-      onClick: function () {
-        //Fn for clicking button
-        MessageToast.show("Thank you for clicking me!");
-      },
-
       onInit: function () {
         const oData = {
           recipient: {
             name: "Helen Quang",
           },
         };
-
         const oModel = new JSONModel(oData);
-
-        //Set data model to the view
         this.getView().setModel(oModel);
+
+        //Set i18n model to the view
+        const i18nModel = new ResourceModel({
+          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+          supportedLocales: [""],
+          fallbackLocale: "",
+        });
+        this.getView().setModel(i18nModel, "i18n");
+      },
+
+      onClick: function () {
+        //Read message from i18n model
+        const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        const sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+        //Show message
+        MessageToast.show(sMsg);
       },
     });
   }
