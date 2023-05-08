94c85512ed79e262aba6581acd0c4d6046d32a5d
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-05-08 13:51:08 +0300

    Add Component Container

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -9,24 +9,6 @@ sap.ui.define(
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-      onInit: function () {
-        const oData = {
-          recipient: {
-            name: "Helen Quang",
-          },
-        };
-        const oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
-
-        //Set i18n model to the view
-        const i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocale: "",
-        });
-        this.getView().setModel(i18nModel, "i18n");
-      },
-
       onClick: function () {
         //Read message from i18n model
         const oBundle = this.getView().getModel("i18n").getResourceBundle();
