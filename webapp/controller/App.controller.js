2bd05d7e92b9e46470ccc74519e682f722e3d1dd
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-05-08 13:01:51 +0300

    Add JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,29 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
       onClick: function () {
+        //Fn for clicking button
         MessageToast.show("Thank you for clicking me!");
+      },
+
+      onInit: function () {
+        const oData = {
+          recipient: {
+            name: "Helen Quang",
+          },
+        };
+
+        const oModel = new JSONModel(oData);
+
+        //Set data model to the view
+        this.getView().setModel(oModel);
       },
     });
   }
