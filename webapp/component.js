c327762962c8e679c5267a6ecdc3982da458b7bf
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-05-08 14:33:49 +0300

    Add configuration

--- webapp/component.js
+++ webapp/component.js
@@ -6,34 +6,24 @@ sap.ui.define(
   ],
   function (UIComponent, JSONModel, ResourceModel) {
     "use strict";
+
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
       metadata: {
-        rootView: {
-          viewName: "sap.ui.demo.walkthrough.view.App",
-          type: "XML",
-          async: true,
-          id: "app",
-        },
+        manifest: "json",
       },
+
       init: function () {
-        // call the init function of the parent
+        // Call the init function of the UI parent
         UIComponent.prototype.init.apply(this, arguments);
-        // set data models
+
+        // Set data JSON models
         var oData = {
           recipient: {
-            name: "UI5",
+            name: "Helen Quang",
           },
         };
         var oModel = new JSONModel(oData);
         this.setModel(oModel);
-
-        // set i18n model
-        var i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocale: "",
-        });
-        this.setModel(i18nModel, "i18n");
       },
     });
   }
