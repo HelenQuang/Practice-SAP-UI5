ff0098a091bc5a4a7155b72a5f482d8f322eae0a
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-05-10 12:19:05 +0300

    Custom formatter

--- webapp/controller/Invoice.controller.js
+++ webapp/controller/Invoice.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel'],
-  function (Controller, JSONModel) {
+  [
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel',
+    '../model/Formatter',
+  ],
+  function (Controller, JSONModel, Formatter) {
     'use strict';
 
     return Controller.extend('sap.ui.demo.walkthrough.controller.InvoiceList', {
@@ -10,6 +14,7 @@ sap.ui.define(
         });
         this.getView().setModel(oViewModel, 'view');
       },
+      formatter: Formatter,
     });
   }
 );
