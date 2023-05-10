959b1bf46afc456c01f3ccf33abc62509cd43653
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-05-10 12:42:07 +0300

    Add filtering

--- webapp/controller/Invoice.controller.js
+++ webapp/controller/Invoice.controller.js
@@ -3,8 +3,10 @@ sap.ui.define(
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
     '../model/Formatter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
   ],
-  function (Controller, JSONModel, Formatter) {
+  function (Controller, JSONModel, Formatter, Filter, FilterOperator) {
     'use strict';
 
     return Controller.extend('sap.ui.demo.walkthrough.controller.InvoiceList', {
@@ -15,6 +17,21 @@ sap.ui.define(
         this.getView().setModel(oViewModel, 'view');
       },
       formatter: Formatter,
+      onFilterInvoices: function (oEvent) {
+        //Build filter array based on query input
+        const aFilter = [];
+        const sQuery = oEvent.getParameter('query');
+        if (sQuery) {
+          aFilter.push(
+            new Filter('ProductName', FilterOperator.Contains, sQuery)
+          );
+        }
+
+        //Bind filter array to the invoice list
+        const oList = this.byId('invoiceList');
+        const oBinding = oList.getBinding('items');
+        oBinding.filter(aFilter);
+      },
     });
   }
 );
